8b46a31ee8198cb92aab9310163eef962c737a38
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2024-10-04 12:43:38 -0500

    👷 Probar despliegue de desarrollo

--- fuente/utilidades/constantes.ts
+++ fuente/utilidades/constantes.ts
@@ -1,1 +1,1 @@
-export const apiBase = 'http://localhost:4040'; // 'https://historiasinternetpre.uniandes.edu.co'; 'http://localhost:4040';
+export const apiBase = import.meta.env.PROD ? 'https://historiasinternetpre.uniandes.edu.co' : 'http://localhost:4040'; // 'https://historiasinternetpre.uniandes.edu.co'; 'http://localhost:4040';
