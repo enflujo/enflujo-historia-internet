1d847da92d720facaacfb42da121c0d288b9ca0c
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2024-10-08 08:08:39 -0500

    🔥 Nuevo servidor de desarrollo

--- aplicaciones/www/fuente/utilidades/constantes.ts
+++ aplicaciones/www/fuente/utilidades/constantes.ts
@@ -1,1 +1,1 @@
-export const apiBase = import.meta.env.PROD ? 'https://historiasinternetpre.uniandes.edu.co' : 'http://localhost:4040'; // 'https://historiasinternetpre.uniandes.edu.co'; 'http://localhost:4040';
+export const apiBase = 'https://wordpress.enflujo.com'; // import.meta.env.PROD ? 'https://historiasinternetpre.uniandes.edu.co' : 'http://localhost:4040';
