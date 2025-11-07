e4647fcc2578f448f01d2982acf0c4d24b3d3d4f
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2025-11-07 09:59:00 -0500

    🐛

--- aplicaciones/www/fuente/utilidades/constantes.ts
+++ aplicaciones/www/fuente/utilidades/constantes.ts
@@ -1,4 +1,4 @@
-export const apiBase = 'https://historiasinternet.uniandes.edu.co'; // import.meta.env.PROD ? 'https://historiasinternetpre.uniandes.edu.co' : 'http://localhost:4040';
+export const apiBase = 'https://historiasinternetpre.uniandes.edu.co'; // import.meta.env.PROD ? 'https://historiasinternetpre.uniandes.edu.co' : 'http://localhost:4040';
 export const base = import.meta.env.BASE_URL;
 
 export const opcionesFecha: Intl.DateTimeFormatOptions = {
